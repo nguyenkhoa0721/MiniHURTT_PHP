53c5942e82d5c5602502ceab5377b744d78f8db7
Nguyen Khoa <nguyenkhoa0721@gmail.com>
2021-08-05 21:36:48 +0700

    add vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;;
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import homepage from './components/homepage.vue';
+import edit from './components/edit.vue';
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        component: homepage
+    },
+    {
+        path: '/edit',
+        component: edit
+    }
+]
+
+const router = new VueRouter({ mode: 'history', routes: routes })
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/js/components/edit.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <h1>Edit</h1>
+</template>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+
+<script>
+    export default{
+    }
+</script>
